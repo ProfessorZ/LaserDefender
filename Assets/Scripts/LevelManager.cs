0f08678684d46aea966ce8d853705c2982ecea15
ProfessorZ <tom.geldermans@gmail.com>
2015-11-12 23:35:55 +0000

    Made basic start screen

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadNewLevel(string levelName){
+		Application.LoadLevel(levelName);
+	}
+
+	public void Quit(){
+		Application.Quit();
+	}
+}

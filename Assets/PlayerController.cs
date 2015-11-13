38cd293a719d30845aed87a5b52b8a7248f51491
ProfessorZ <tom.geldermans@gmail.com>
2015-11-13 00:04:07 +0000

    Playership can now be moved left and right with 'horizontal' axis, being left and right arrow or eg Xbox 360 L stick.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed; // Speed multiplier for player ship
+	float xMin, xMax; // boundaries
+	public float padding; // boundaries + padding = playspace
+
+	// Use this for initialization
+	void Start () {
+		xMin = Camera.main.ViewportToWorldPoint(new Vector3(0,0,-10)).x + padding;
+		xMax = Camera.main.ViewportToWorldPoint(new Vector3(1,0,-10)).x - padding;
+		Debug.Log(xMin + " " + xMax);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(Input.GetAxis("Horizontal"));
+		Vector3 position = transform.position + new Vector3(Input.GetAxis("Horizontal")*speed*Time.deltaTime,0,0);
+		transform.position = new Vector3(Mathf.Clamp(position.x,xMin,xMax),transform.position.y,transform.position.z);
+	}
+}

747314a2e1c15e58c8d91a23f7b8dbfec76eebed
ProfessorZ <tom.geldermans@gmail.com>
2015-11-14 17:15:17 +0000

    corrected code.

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -5,8 +5,8 @@ public class EnemySpawner : MonoBehaviour {
 	public GameObject enemyPrefab;
 	// Use this for initialization
 	void Start () {
-		GameObject enemy = Instantiate(enemyPrefab,new Vector3(0,3,0),Quaternion.identity) as GameObject; 
-		enemy.transform = this.transform;
+		GameObject enemy = Instantiate(enemyPrefab,new Vector3(0,3,0),Quaternion.identity) as GameObject;  // instantiate new Enemy GameObject
+		enemy.transform.parent = transform; // put Enemy instance under EnemyFormation GO
 	}
 	
 	// Update is called once per frame

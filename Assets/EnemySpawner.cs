89aa4f5e60ac058e91de55c3fd24f80da6b03e9f
ProfessorZ <tom.geldermans@gmail.com>
2015-11-14 17:38:23 +0000

    Enemies now spawn under their positions, under an organizational GO called EnemyFormation. Enemies spawn programmatically.

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -5,8 +5,12 @@ public class EnemySpawner : MonoBehaviour {
 	public GameObject enemyPrefab;
 	// Use this for initialization
 	void Start () {
-		GameObject enemy = Instantiate(enemyPrefab,new Vector3(0,3,0),Quaternion.identity) as GameObject;  // instantiate new Enemy GameObject
-		enemy.transform.parent = transform; // put Enemy instance under EnemyFormation GO
+
+
+		foreach (Transform position in transform) {
+			GameObject enemy = Instantiate(enemyPrefab,position.transform.position,Quaternion.identity) as GameObject;  // instantiate new Enemy GameObject
+			enemy.transform.parent = position; // put Enemy instance under EnemyFormation GO
+		}
 	}
 	
 	// Update is called once per frame
